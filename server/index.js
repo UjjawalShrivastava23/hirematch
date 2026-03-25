092f3fa8cb07f5bdf1e0b70c8182c2e9bdf69226
UjjawalShrivastava23 <ujjawalshri2019@gmail.com>
2026-03-25 12:54:48 +0530

    fix port and db connection

--- server/index.js
+++ server/index.js
@@ -20,5 +20,8 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log("DB ERROR:", err));
 
 
