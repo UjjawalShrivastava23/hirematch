758a85efe7eaae7377a5ba3b6ddf6903dee24c56
UjjawalShrivastava23 <ujjawalshri2019@gmail.com>
2026-03-25 19:41:44 +0530

    updated baclend url

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,7 +8,7 @@ function Login(){
  
      async function handlesubmit(){
     try{
-    const response = await axios.post('http://localhost:8000/api/auth/login', {
+    const response = await axios.post('https://hirematch-backend.onrender.com/api/auth/login', {
   email: email,
   password: password
 })
